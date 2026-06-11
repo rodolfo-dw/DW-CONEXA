93cbb80c04f8b79a7d346cf40eb69eb6a6089cbb
Rodolfo Vasconcelos <rodolfo.denardi@hotmail.com>
2026-06-11 17:38:26 -0300

    removido legado

--- 1. base de conhecimento/components/gestor-chamados-rest/src/main/java/br/com/userflow/fluig/rest/api/Swagger.java
+++
@@ -1,9 +0,0 @@
-package br.com.userflow.fluig.rest.api;
-
-import javax.ws.rs.Path;
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-
-@Path("/openapi.{type:json|yaml}")
-public class Swagger extends OpenApiResource {
-
-}
--- 1. base de conhecimento/components/gestor-chamados-rest/src/main/java/br/com/userflow/fluig/rest/util/fluig/soap/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.workflow.ecm.technology.totvs.com/")
-package br.com.userflow.fluig.rest.util.fluig.soap.generated;
